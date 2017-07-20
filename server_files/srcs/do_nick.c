95afa46979535032d50762d06535b5e10f91dad1
Kalilo <kalilohansmann@gmail.com>
2017-07-20 11:05:33 +0200

    Added basic nick changer

--- server_files/srcs/do_nick.c
+++ server_files/srcs/do_nick.c
@@ -12,7 +12,18 @@
 
 #include "../includes/server.h"
 
-char	do_nick(int client, char *nick)
+/*
+** Very basic, need to be upgraded
+*/
+
+char	do_nick(int sd, char *nick)
 {
+	if (ft_strlen(nick) > 9)
+	{
+		MSG_ERROR = "Nicknames have to be less than 10 characters.";
+		return (0);
+	}
+	ft_strdel(&CLIENT(sd).nick);
+	CLIENT(sd).nick = ft_strdup(nick);
 	return (1);
 }
