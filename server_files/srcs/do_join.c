7a42d5a40e675d1eb1a1b0bda46bc0d2bf4313d1
Kalilo <kaliohansmann@gmail.com>
2017-07-20 19:55:17 +0200

    began working on channels

--- server_files/srcs/do_join.c
+++ server_files/srcs/do_join.c
@@ -14,7 +14,34 @@
 
 char	do_join(int sd, char *channel)
 {
-	(void)sd;
-	(void)channel;
+	t_list		*chan;
+	t_list		*user;
+
+	chan = g_env.channels;
+	while (chan && ft_strcmp(channel, chan->name))
+		chan = chan->next;
+	if (!chan)
+	{
+		MSG_ERROR = "Channel doesn't exist, or isn't avaliable.";
+		return (0);
+	}
+	user = chan->users;
+	while (user && user->next)
+	{
+		if (!ft_strcmp(user->content.name, CLIENT(sd).name))
+		{
+			MSG_ERROR = "Already in channel.";
+			return (0);
+		}
+		user = user->next;
+	}
+	if (user)
+	{
+		if (!(user->next = ft_lstnew(CLIENT(sd), sizeof(t_user))))
+			error_quit("failed to allocate memory.");
+	}
+	else
+		if (!(chan->users = ft_lstnew(CLIENT(sd), sizeof(t_user))))
+			error_quit("failed to allocate memory.");
 	return (1);
 }
--- server_files/srcs/do_leave.c
+++ server_files/srcs/do_leave.c
@@ -14,7 +14,26 @@
 
 char	do_leave(int sd, char *channel)
 {
-	(void)sd;
-	(void)channel;
+	t_list		*chan;
+	t_list		*user;
+
+	chan = g_env.channels;
+	while (chan && ft_strcmp(channel, chan->name))
+		chan = chan->next;
+	if (!chan)
+	{
+		MSG_ERROR = "Channel doesn't exist, or isn't avaliable.";
+		return (0);
+	}
+	user = chan->users;
+	while (user && user->next)
+	{
+		if (!ft_strcmp(user->next->content.name, CLIENT(sd).name))
+		{
+			MSG_ERROR = "Already in channel.";
+			return (0);
+		}
+		user = user->next;
+	}
 	return (1);
 }
