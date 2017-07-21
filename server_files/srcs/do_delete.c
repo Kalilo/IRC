676f8443be61a9ba81041d745df67c2fa0b90d89
Kalilo <kalilohansmann@gmail.com>
2017-07-21 11:59:28 +0200

    Rewrote to use new lst functions

--- server_files/srcs/do_delete.c
+++ server_files/srcs/do_delete.c
@@ -21,32 +21,30 @@ char	do_delete(char *channel)
 	t_list		*chan;
 	t_list		*tmp;
 
-	chan = g_env.channels;
-	if (chan && chan->next && !ft_strcmp(channel,
-		((t_channel *)(chan->next))->name))
+	if ((chan = find_channel_parent(channel)))
 	{
-		g_env.channels = chan->next;
-		free(chan->content);
-		free(chan);
-		return (1);
+		if (((t_channel *)(chan->content))->users)
+		{
+			MSG_ERROR = ft_strdup(MSG_E04);
+			return (0);
+		}
+		tmp = chan;
+		chan->next = chan->next->next;
+		free(tmp->content);
+		free(tmp);
 	}
-	while (chan && chan->next &&
-			ft_strcmp(channel, ((t_channel *)(chan->next->content))->name))
-		chan = chan->next;
-	if (!chan || !chan->next || ft_strcmp(channel,
-		((t_channel *)(chan->next->content))->name))
+	else if (g_env.channels &&
+		!ft_strcmp(((t_channel *)(g_env.channels))->name, channel))
+	{
+		tmp = g_env.channels;
+		g_env.channels = tmp->next;
+		free(tmp->content);
+		free(tmp);
+	}
+	else
 	{
 		MSG_ERROR = ft_strdup(MSG_E03);
 		return (0);
 	}
-	if (((t_channel *)(chan->content))->users)
-	{
-		MSG_ERROR = ft_strdup(MSG_E04);
-		return (0);
-	}
-	tmp = chan->next;
-	chan->next = chan->next->next;
-	free(tmp->content);
-	free(tmp);
 	return (1);
 }
