ccf64f608a948023ca37148dad164626aecc4fe1
Kalilo <kalilohansmann@gmail.com>
2017-07-22 09:22:02 +0200

    Coded basic message sender

--- server_files/srcs/do_msg.c
+++ server_files/srcs/do_msg.c
@@ -12,9 +12,36 @@
 
 #include "../includes/server.h"
 
-char	do_msg(int sd, char *msg_details)
+char		do_msg(int sd, char *msg_details)
 {
-	(void)sd;
-	(void)msg_details;
+	t_msg		msg;
+	int			k;
+	t_list		*chan;
+
+	if (!parse_message(&msg, msg_details) && clear_msg(&msg))
+		return (0);
+	k = -1;
+	if (msg.type == msg_direct)
+	{
+		while (++k < MAX_CLIENTS)
+			if (CLIENT_SOCK(k) && !ft_strcmp(CLIENT(k).nick, msg.dest))
+			{
+				send(CLIENT_SOCK(k), msg.msg, ft_strlen(msg.msg),
+					MSG_DONTWAIT);
+				break ;
+			}
+	}
+	else
+	{
+		chan = find_channel(msg.dest);
+		chan = ((t_channel *)(chan->content))->users;
+		while (chan)
+		{
+			send((((t_user *)(chan->content))->sock), msg.msg,
+				ft_strlen(msg.msg), MSG_DONTWAIT);
+			chan = chan->next;
+		}
+	}
+	clear_msg(&msg);
 	return (1);
 }
